d00977ffe6772da9895211aea078feac77feb147
JokoFlex <christopher.haindl06@gmail.com>
2024-05-31 00:54:02 +0200

    Added some type safety, and possible null-crashes hihi, and added npm/yarn installation

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,15 +1,15 @@
 import { JSDOM } from 'jsdom';
 import {ITeacher} from "./models/ITeacher";
 
-function extractTitleAndName(nameElement: HTMLAnchorElement): string[]
+function extractTitleAndName(nameElement: Element): string[]
 {
     const contents: string[] = nameElement.innerHTML.split(/(?=[\S])<!-- -->/)
 
-    let prefix: string = undefined
+    let prefix: string = ""
     if(contents[1] === " ")
         prefix = contents[0]
 
-    return [prefix, contents[prefix === undefined ? 1 : 2], contents[prefix === undefined ? 2 : 3]]
+    return [prefix, contents[prefix === "" ? 1 : 2], contents[prefix === "" ? 2 : 3]]
 }
 
 export async function getTeachersOfKaindorf(): Promise<ITeacher[]> {
@@ -21,14 +21,14 @@ export async function getTeachersOfKaindorf(): Promise<ITeacher[]> {
 
             const root = dom.window.document.body.getElementsByTagName("section")[0]
             const divs = root.children
-            const teacherDivs: HTMLDivElement[] = Array.from(divs[divs.length - 1].children)
+            const teacherDivs: Element[] = Array.from(divs[divs.length - 1].children)
 
             // Get data for regular teachers
             for (const teacherDiv of teacherDivs) {
                 const children: HTMLCollectionOf<HTMLDivElement> = teacherDiv.getElementsByTagName("div")
-                const imgUrl: string = decodeURIComponent(children[0].getElementsByTagName("img")[0].getAttribute("srcset").split("url=")[1].split("&w=")[0])
+                const imgUrl: string = decodeURIComponent(children[0].getElementsByTagName("img")[0]!.getAttribute("srcset")!.split("url=")[1].split("&w=")[0])
 
-                const mail: string = children[1].getElementsByTagName("a")[0].getAttribute("href").split(":")[1]
+                const mail: string = children[1].getElementsByTagName("a")[0]!.getAttribute("href")!.split(":")[1]
                 const shorthand: string = children[1].getElementsByTagName("div")[0].innerHTML.split(">")[1].split("<")[0].toUpperCase()
                 const names: string[] = extractTitleAndName(children[1].getElementsByTagName("a")[0])
 
@@ -46,9 +46,9 @@ export async function getTeachersOfKaindorf(): Promise<ITeacher[]> {
             for (let i: number = 2; i < divs.length - 4; i += 2) {
                 while (i === 3 || i === 4)
                     i++
-                const imgUrl: string = decodeURIComponent(divs[i].getElementsByTagName("div")[0].getElementsByTagName("img")[0].getAttribute("srcset").split("url=")[1].split("&w=")[0])
+                const imgUrl: string = decodeURIComponent(divs[i].getElementsByTagName("div")![0].getElementsByTagName("img")![0].getAttribute("srcset")!.split("url=")[1].split("&w=")[0])
 
-                const mail: string = divs[i + 1].getElementsByTagName("a")[0].getAttribute("href").split(":")[1]
+                const mail: string = divs[i + 1].getElementsByTagName("a")[0].getAttribute("href")!.split(":")[1]
                 const shorthand: string = divs[i + 1].getElementsByTagName("span")[0].innerHTML.split(">")[1].split("<")[0].toUpperCase()
                 const names: string[] = extractTitleAndName(divs[i + 1].getElementsByTagName("div")[1])
 
