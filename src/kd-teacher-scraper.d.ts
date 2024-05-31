a3d15ef1cee23992e66c6beef8c9f21c68930a69
JokoFlex <christopher.haindl06@gmail.com>
2024-05-31 11:51:07 +0200

    installable fix

--- src/kd-teacher-scraper.d.ts
+++
@@ -1,6 +0,0 @@
-import {ITeacher} from "./models/ITeacher";
-import getTeachersOfKaindorf from "./kd-teacher-scraper";
-
-declare module 'kd-teacher-scraper' {
-    export default getTeachersOfKaindorf;
-}
